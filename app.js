74e17d996916f98ad2106b4954de4a68b12d4e47
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-22 12:10:07 -0700

    Fix inquirer version.

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
 
+const mysqlHost = process.env.MYSQL_HOST || 'localhost';
+const mysqlPort = process.env.MYSQL_PORT || '3306';
+const mysqlUser = process.env.MYSQL_USER || 'root';
+const mysqlDatabase = process.env.MYSQL_SCHEMA || 'employees';
+
 const conn = mysql.createConnection(
   {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PW,
-    table: 'employees'
+      host: mysqlHost,
+      port: mysqlPort,
+      user: mysqlUser,
+      password: process.env.MYSQL_PW,
+      database: mysqlDatabase
   },
-  console.log('Connecting to MySQL on localhost port 3306 user root.')
+  console.log(`Connected to MySQL on ${mysqlHost} port ${mysqlPort} user ${mysqlUser} using database ${mysqlDatabase}.`)
 )
