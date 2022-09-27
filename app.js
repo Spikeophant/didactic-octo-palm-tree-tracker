96bc5d0119a8e89638e15a209653b35c9822cd3f
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-27 13:31:32 -0700

    New questions and inquirer prompts to add additional objects to db.

--- app.js
+++ app.js
@@ -13,23 +13,76 @@ const questions = require('./lib/questions');
       db.query('SELECT * FROM employee', '', (err, employees) => {
         if (!err) {
           console.log('Congratulations, you have successfully selected view all employees!')
-          for (const employee of employees) {
-            console.log(`Employee identifier: ${employee.id}
-Employee first name: ${employee.first_name}
-Employee last name: ${employee.last_name}
-Employee role identifier: ${employee.role_id}
-Managerial reporting identifier: ${employee.manager_id}`);
-          }
+            console.table(employees);
           return optionsMenu();
         } else console.log(err)
       })
     } else if (answers.options === 'View all departments.') {
-            console.log('You have selected all departments yay!  Thank you for your selection, please view below!')
-      console.log('We should probably implement the customers requests.')
+      db.query('SELECT * FROM department', '', (err, departments) => {
+        if (!err) {
+          console.log('You have selected all departments yay!  Thank you for your selection, please view below!');
+          console.table(departments)
+        } else {
+          console.log(err);
+          process.exit(1)
+      }
+      })
       return optionsMenu();
-          }
+    } else if (answers.options === 'View all roles.') {
+      db.query('SELECT * FROM role', '',
+        (err, roles) => {
+        if(!err) {
+          console.log('You have selected to view all roles, please enjoy your response.');
+          console.table(roles);
+        } else {
+          console.log(err);
+          process.exit(1);
+        }
 
       })
-    })();
+      return optionsMenu()
+    } else if (answers.options === 'Add a department.') {
+      inquirer.prompt(questions.addDepartment).then((answers) => {
+        db.query('INSERT INTO department (name) VALUES (?)', answers.name,
+          (err, data) => {
+          if (err) {
+            console.log(err);
+            process.exit(1)
+          } else {
+            return optionsMenu();
+          }
+        });
+      })
+    } else if (answers.options === 'Add a role.') {
+      inquirer.prompt(questions.addRole).then((answers) => {
+        db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+          [answers.title, answers.salary, answers.deptId],
+          (err, data_)=> {
+          if (err) {
+            console.log(err);
+            process.exit(0);
+          } else {
+            return optionsMenu();
+          }
+        })
+      })
+    } else if (answers.options === 'Add an employee.') {
+      inquirer.prompt(questions.addEmployee).then( (answers) => {
+        db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+          [answers.first_name, answers.last_name, answers.role_id, answers.manager_id],
+          (err, result) => {
+          if (err) {
+            console.log(err);
+            process.exit(1);
+          } else {
+            return optionsMenu();
+          }
+          })
+      })
+    }
+
+      })
+    })
+();
 
 
